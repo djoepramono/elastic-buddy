e35ee8d860c713ef0c8a666433e096fafb0cbaff
Djoe Pramono <djoepramono@gmail.com>
2020-11-16 19:07:35 +1100

    source type from npm modules instead of manually write them

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Client } from '@elastic/elasticsearch';
+import { SearchResponse } from 'elasticsearch';
 
 const client = new Client({ node: 'http://localhost:9200' });
 
@@ -9,53 +10,14 @@ interface SearchBody {
     }
 }
 
-// Complete definition of the Search response
-interface ShardsResponse {
-    total: number;
-    successful: number;
-    failed: number;
-    skipped: number;
-}
-
-interface Explanation {
-    value: number;
-    description: string;
-    details: Explanation[];
-}
-
-interface SearchResponse<T> {
-    took: number;
-    timed_out: boolean;
-    _scroll_id?: string;
-    _shards: ShardsResponse;
-    hits: {
-      total: number;
-      max_score: number;
-      hits: Array<{
-        _index: string;
-        _type: string;
-        _id: string;
-        _score: number;
-        _source: T;
-        _version?: number;
-        _explanation?: Explanation;
-        fields?: any;
-        highlight?: any;
-        inner_hits?: any;
-        matched_queries?: string[];
-        sort?: string[];
-      }>;
-    };
-    aggregations?: any;
-}
-
-// Define the interface of the source object
 interface Source {
-    title: string
+    title: string,
+    year: number,
+    tag: string
 }
 
 const run = async () => {
-    console.log('dsds');    
+    console.log('dsds');
     const response = await client.search<SearchResponse<Source>, SearchBody>({
         index: 'movie',
         body: {
@@ -65,8 +27,9 @@ const run = async () => {
                 },
             },
         },
-    });    
-    console.log(response.body.hits.hits[0]._source);
+    });
+    const firstResult = (response.body.hits.hits[0]._source);
+    console.log(`Found ${firstResult.title} from ${firstResult.year}`);
 };
 
 run().catch(console.log);
